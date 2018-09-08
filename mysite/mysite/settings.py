ce7dfe670171c92596ed0786d6a535555ba80d2d
Hans Tananda <hanstananda@gmail.com>
2018-09-08 23:30:51 +0800

    finished tutorial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,11 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            # try to hard-code
+            'D:\\Programming\\Website\\blog_test\\mysite\\templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
+    url(r'^polls/', include('polls.urls')),  # this line added
     path('admin/', admin.site.urls),
 ]
