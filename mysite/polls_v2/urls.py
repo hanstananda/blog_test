c44bf9354a6372cb02059b08bd8945c0a0d4f9cc
Hans Tananda <hanstananda@gmail.com>
2018-09-10 02:10:16 +0800

    checkpoint for finishing tutorial 4

--- mysite/polls_v2/urls.py
+++ mysite/polls_v2/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'polls_v2'
 
+"""
 urlpatterns = [
     # ex: /polls_v2/
     path('', views.index, name='index'),
@@ -15,4 +16,13 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls_v2/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+]"""
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# NOTE! pk here means primary key!! It searches on primary key!
--- mysite/polls_v2/views.py
+++ mysite/polls_v2/views.py
@@ -1,54 +1,28 @@
-from django.http import HttpResponse
-from .models import Question, Choice
-
-
-
-"""def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)"""
-
-"""  # update from code above
-from django.template import loader  # added for html template load
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls_v2/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))"""
-
-from django.shortcuts import render  # shortcut to render the context
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls_v2/index.html', context)
-
-
-"""
-from django.http import Http404
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls_v2/detail.html', {'question': question})"""
-# Alternative for detail below, see https://docs.djangoproject.com/en/2.1/intro/tutorial03/ for more info
-
-
-from django.shortcuts import get_object_or_404
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls_v2/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls_v2/results.html', {'question': question})
-
-
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls_v2/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls_v2/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls_v2/results.html'
 
 
 def vote(request, question_id):
