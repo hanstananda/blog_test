6b1a3728b58c98a3d42f232145216b1109276942
Hans Tananda <hanstananda@gmail.com>
2018-09-11 02:35:04 +0800

    added keys for Post, remake database

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,11 +13,16 @@ class Category(models.Model):
         ordering = ["-category_name"]
 
 
+class User(models.Model):
+    user_name = models.CharField(max_length=25)
+
+
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     post_content = models.CharField(max_length=100000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    User = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.post_title
