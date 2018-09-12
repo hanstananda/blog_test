b35979d05c504498d510caf4a3569081fe2fbd2d
Hans Tananda <hanstananda@gmail.com>
2018-09-12 16:32:08 +0800

    migrated db, reset db, changed UserProfile

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -19,13 +19,13 @@ class UserProfile(models.Model):
     user_image = models.ImageField(default=None, blank=True, null=True)
 
     def __str__(self):
-        return self.user_name
+        return self.user_name.username
 
 
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    post_content = models.CharField(max_length=100000)
+    post_content = models.TextField(max_length=100000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     User = models.ForeignKey(User, on_delete=models.CASCADE)
 
